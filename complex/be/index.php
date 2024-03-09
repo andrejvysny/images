acb81d4128ea25591951a690737a2e799cc0d790
AndrejVysny <vysnyandrej@gmail.com>
2024-03-09 01:10:13 +0100

    test

--- complex/be/index.php
+++ complex/be/index.php
@@ -6,7 +6,5 @@ header("Access-Control-Allow-Origin: *");
 // Initialize an empty array to store items
 $items = ['message'=>($_ENV['MESSAGE'] ?? 'No MESSAGE variable in ENV'), 'port'=>$_SERVER['SERVER_PORT'], 'SERVER_ADDR'=> $_SERVER['SERVER_ADDR'], 'SERVER_NAME'=> $_SERVER['SERVER_NAME'] ];
 
-
 echo json_encode($items);
-
 ?>
